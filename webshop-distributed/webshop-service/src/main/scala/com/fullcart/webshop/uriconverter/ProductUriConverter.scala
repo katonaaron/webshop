9be2b8e11665e2dc2d66098a1ec2da4b7f31b0c8
Katona Áron <katonaaron01@gmail.com>
2021-01-01 17:37:57 +0200

    Remove unused classes and dependencies

--- webshop-distributed/webshop-service/src/main/scala/com/fullcart/webshop/uriconverter/ProductUriConverter.scala
+++
@@ -1,8 +0,0 @@
-package com.fullcart.webshop.uriconverter
-
-import com.fullcart.webshop.model.Product
-import com.fullcart.webshop.repository.ProductRepository
-import org.springframework.stereotype.Component
-
-@Component
-class ProductUriConverter(private val repository: ProductRepository) extends AbstractUriConverter[Product]("products", repository)
--- webshop-distributed/webshop-service/src/main/scala/com/fullcart/webshop/uriconverter/UriConverter.scala
+++
@@ -1,8 +0,0 @@
-package com.fullcart.webshop.uriconverter
-
-import java.net.URI
-
-trait UriConverter[T] {
-
-  def convert(uri: URI): Option[T]
-}
